80d71b17d8ea8e6f325b553b9fd9565b44fae55b
pallavi149 <5811@holbertonstudents.com>
2023-01-16 17:57:48 -0800

    Fixed Error

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,4 @@
 for alpha_letters in range(97, 123):
       if alpha_letters == ord('e') or alpha_letters == ord('q'):
             continue
-      print("{:c}".format(alpha_letters), end = "")
+      print("{:c}".format(alpha_letters), end="")
