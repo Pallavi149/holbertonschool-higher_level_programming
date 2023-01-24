502d5fbfe1b58615f8185fbce14e6f2362f2be0e
pallavi149 <5811@holbertonstudents.com>
2023-01-23 23:07:18 -0800

    Fixed return statement if string is empty

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -3,6 +3,7 @@ def multiple_returns(sentence):
     my_string = sentence
     length = len(my_string)
     if my_string is None:
-        return None
+        length = 0
+        return (length, None)
     else:
         return (length, my_string[0])
