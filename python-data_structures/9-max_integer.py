5ccd0e199ab692e95dc2cb9dcd73968d6de13f6e
pallavi149 <5811@holbertonstudents.com>
2023-01-24 16:16:27 -0800

    Assigned max to -1

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     if not my_list:
         return None
     else:
-        max = 0
+        max = -1
         for i in my_list:
             if i > max:
                 max = i
