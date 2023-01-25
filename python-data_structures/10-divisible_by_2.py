abee0e3bb45dc2531260c6ce9e316dbc67ab350e
pallavi149 <5811@holbertonstudents.com>
2023-01-24 21:07:03 -0800

    changed for loop

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     if not my_list:
         return my_list
     new_list = list(my_list)
-    for item in new_list:
+    for item in range(len(new_list)):
         if item % 2 == 0:
             new_list[item] = True
         else:
