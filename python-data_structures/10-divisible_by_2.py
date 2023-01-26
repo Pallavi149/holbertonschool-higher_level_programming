95db7522a4a7f7671da4fcf20ea358eaa3061b1c
pallavi149 <5811@holbertonstudents.com>
2023-01-26 15:31:15 -0800

    Updated if statement

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    if not my_list:
-        return my_list
-    new_list = list(my_list)
+    new_list = my_list.copy()
     for item in range(len(new_list)):
-        if item % 2 == 0:
+        if new_list[item] % 2 == 0:
             new_list[item] = True
         else:
             new_list[item] = False
