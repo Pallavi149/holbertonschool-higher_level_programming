bf54ed6ccc6c41c75d5add2d05f15942e6f55685
pallavi149 <5811@holbertonstudents.com>
2023-01-27 23:27:28 -0800

    Changed variable name

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,10 +2,10 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    biggest = 0
+    biggest_score = 0
     for key, value in a_dictionary.items():
-        if value > biggest:
-            biggest = value
+        if value > biggest_score:
+            biggest_score = value
     for key, value in a_dictionary.items():
-        if value == biggest:
+        if value == biggest_score:
             return key
