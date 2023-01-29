79100b87fde275099d34af5cff47e1f612f46317
pallavi149 <5811@holbertonstudents.com>
2023-01-29 04:56:36 -0800

    roman to int

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -10,9 +10,7 @@ def roman_to_int(roman_string):
     # Dictionary for roman numerals
     r_dict = {
         "I": 1,
-        "IV": 4,
         "V": 5,
-        "IX": 9,
         "X": 10,
         "L": 50,
         "C": 100,
@@ -20,26 +18,13 @@ def roman_to_int(roman_string):
         "M": 1000
     }
 
+
     result = 0
-    temp = list(roman_string)
-    # Concat 4 and 9s
-    if len(temp) > 1:
-        idx = 0
-        for i in temp:
-            try:
-                if temp[idx] == 'I' and temp[idx + 1] == 'V':
-                    temp[idx:idx + 2] = [''.join(temp[idx:idx + 2])]
-            except IndexError:
-                pass
-            try:
-                if temp[idx] == 'I' and temp[idx + 1] == 'X':
-                    temp[idx:idx + 2] = [''.join(temp[idx:idx + 2])]
-            except IndexError:
-                pass
-            idx += 1
-    # Search in dict for correct numbers and add
-    for k, v in r_dict.items():
-        for index in temp:
-            if index == k:
-                result += v
+    length = len(roman_string)
+    for i in range(length):
+        value = r_dict.get(roman_string[i])
+        if i < (length - 1) and value < r_dict.get(roman_string[i + 1]):
+            result  = result - value
+        else:
+            result = result + value
     return result
