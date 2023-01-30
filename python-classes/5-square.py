072f9eb0a92837484fcafabdd23d2e83fbc1da3e
pallavi149 <5811@holbertonstudents.com>
2023-01-30 15:39:30 -0800

    Printing pattern

---
+++ python-classes/5-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Class that defines Square"""
+
+
+class Square:
+    """Class that returns area of square and Prints a pattern"""
+    def __init__(self, size):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            print("#" * self.__size)
