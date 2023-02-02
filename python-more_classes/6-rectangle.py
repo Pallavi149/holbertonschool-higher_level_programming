5ad4ee5a7fa07fa7434bb69eac3a545a1728f545
pallavi149 <5811@holbertonstudents.com>
2023-02-02 02:21:30 -0800

    Number of instances increment and decrement

---
+++ python-more_classes/6-rectangle.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""Defines a class Rectangle"""
+
+
+class Rectangle:
+    """A class for a rectangle of given width and height
+    Attributes:
+    __width (int): the width of the rectangle
+    __height (int): the height of the rectangle
+    """
+
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+        Rectangle.number_of_instances = Rectangle.number_of_instances + 1
+
+    @property
+    def width(self):
+        """Property getter for __width. Returns the width"""
+        return(self.__width)
+
+    @width.setter
+    def width(self, value):
+        """Property setter for __width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """Property getter for __height. Returns the height"""
+        return(self.__height)
+
+    @height.setter
+    def height(self, value):
+        """Property setter for __height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """Public instance method returning the area"""
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """Public instance method returning the perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            pm = 2 * (self.__width + self.__height)
+            return pm
+    def __str__(self):
+        if self.__width != 0 and self.__height != 0:
+            for i in range(self.__height):
+                for j in range(self.__width):
+                    return f'#'
+            print()
+
+    def __repr__(self):
+        return '\n'.join('#' * self.width for _ in range(self.height))
+
+    def __del__(self):
+        print("Bye rectangle...")
+        Rectangle.number_of_instances = Rectangle.number_of_instances - 1
