d13d9927b10dcce465bccaa900ec7337983d6ccd
pallavi149 <5811@holbertonstudents.com>
2023-02-03 16:44:41 -0800

    add integer

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This is "0-add_integer" module, it supllies one function, add_integer().
+"""
+
+def add_integer(a, b=98):
+    """
+    Adds two numbers and returns the result. Second argument to this function is
+    optional and it has default value of 98.
+    """
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    return (a + b)
