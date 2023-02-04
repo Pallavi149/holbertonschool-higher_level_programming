c52aeb33a86bdf124318c7825134bc07cbe7361b
pallavi149 <5811@holbertonstudents.com>
2023-02-04 01:11:37 -0800

    Fixed the error of module documentation and function documentatio

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""
+This is "3-say_my_name" module, it supllies one function, say_my_name().
+"""
+
+
 def say_my_name(first_name, last_name=""):
+    """
+    Function has two string arguments which represents first name and last name
+    second argument 'last_name' is an optional
+    """
 
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
