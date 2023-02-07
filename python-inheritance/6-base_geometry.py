0e617ee6ee41936f684d2a6ef5cff729e5456bcd
pallavi149 <5811@holbertonstudents.com>
2023-02-07 15:56:50 -0800

    public instance method

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module "6-base_geometry" including "BaseGeometry" class
+and public instance method "area"
+"""
+
+class BaseGeometry:
+    def area(self):
+        """public instance method that raises an exception"""
+        raise Exception("area() is not implemented")
