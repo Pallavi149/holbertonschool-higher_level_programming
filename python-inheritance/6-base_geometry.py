6229dbf79c7ed624d3fed53cd70c79e4d3439b26
pallavi149 <5811@holbertonstudents.com>
2023-02-07 16:01:12 -0800

    Updated pycodestyle

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -4,12 +4,11 @@ Module "6-base_geometry" including "BaseGeometry" class
 and public instance method "area"
 """
 
+
 class BaseGeometry:
     """
     A class with public instance method "area"
     """
-
-
     def area(self):
         """public instance method that raises an exception"""
         raise Exception("area() is not implemented")
