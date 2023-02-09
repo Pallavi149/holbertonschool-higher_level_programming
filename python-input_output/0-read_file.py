d941aa97f2685ada1d36ebbf119835af98bee5f6
pallavi149 <5811@holbertonstudents.com>
2023-02-08 16:06:32 -0800

    added end of line in file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -9,4 +9,4 @@ def read_file(filename=""):
     Reads a text file and prints it to stdout
     """
     with open(filename, "r", encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
