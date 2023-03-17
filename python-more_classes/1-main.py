7161ddbf57c050b6189228b89d1f76be098f4ef5
pallavi149 <5811@holbertonstudents.com>
2023-03-16 20:07:02 -0700

    Removing unnecessary files

--- python-more_classes/1-main.py
+++ python-more_classes/1-main.py
@@ -6,4 +6,10 @@ print(my_rectangle.__dict__)
 
 my_rectangle.width = 10
 my_rectangle.height = 3
+my_rectangle.foobar = 5
+my_rectangle.__foobar = 42
+my_rectangle.__width = 57
+print("Before")
+print(my_rectangle.__dict__)
+my_rectangle._Rectangle__width = 13
 print(my_rectangle.__dict__)
