c9e2b92daf38a954f4eeaee69ad4486c758b707a
pallavi149 <5811@holbertonstudents.com>
2023-04-12 16:44:25 -0700

    making function mvisible from outside

---
+++ javascript-warm_up/13-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+function add (a, b) {
+  return (a + b);
+}
+module.exports = add;
+// main file
+// #!/usr/bin/node
+// const add = require('./13-add').add;
+// console.log(add(3, 5));
