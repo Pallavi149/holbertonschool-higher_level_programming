c6958e28b62c208de7e44caac7ebe3521df879f1
pallavi149 <5811@holbertonstudents.com>
2023-04-16 20:50:48 -0700

    fixed documentation error

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const fs = require('fs')
+const fs = require('fs');
 fs.readFile(process.argv[2], (err, inputD) => {
-   if (err) throw err;
-      console.log(inputD.toString());
-})
+  if (err) throw err;
+  console.log(inputD.toString());
+});
