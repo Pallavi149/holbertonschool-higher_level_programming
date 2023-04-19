739645e00f5c17e6c9a7d5ef1e1c7dcf81734045
pallavi149 <5811@holbertonstudents.com>
2023-04-19 15:52:49 -0700

    script that gets the contents of a webpage and stores it in a file

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+request.get(process.argv[2], function(error, response, body) {
+    if (error) {
+	console.log(error);
+    }
+    fs.writeFile(process.argv[3], body, `utf-8`, function (error) {
+	if (error) {
+	    console.log(error);
+	}
+    });
+});
+// node 5-request_store.js http://loripsum.net/api loripsum
